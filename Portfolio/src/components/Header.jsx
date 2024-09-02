830800256531f1be44371edf7b5dfaf899be439b
mdekerga <mathieu.dekergariou@gmail.com>
2024-09-02 10:37:01 +0200

    Update Header.jsx

--- Portfolio/src/components/Header.jsx
+++ Portfolio/src/components/Header.jsx
@@ -8,18 +8,9 @@ export default function Header(){
                     </div>
                     <div className="flex-none">
                         <ul className="menu menu-horizontal px-1">
-                        <li><a href="#competences">COMPETENCES</a></li>
-                        <li><a href="#projets">PROJETS</a></li>
-                        <li><a href="#footer">CONTACT</a></li>
-                        <li>
-                            <details>
-                            <summary>Parent</summary>
-                            <ul className="bg-base-100 rounded-t-none p-2">
-                                <li><a>Link 1</a></li>
-                                <li><a>Link 2</a></li>
-                            </ul>
-                            </details>
-                        </li>
+                            <li><a href="#competences">COMPETENCES</a></li>
+                            <li><a href="#projets">PROJETS</a></li>
+                            <li><a href="#footer">CONTACT</a></li>
                         </ul>
                     </div>
                 </div>
