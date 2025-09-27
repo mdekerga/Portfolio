d2ba56e170c2bfd3b18aa9cfc5ec2f215d702216
mdekerga <mathieu.dekergariou@gmail.com>
2025-09-27 11:47:51 +0200

    new skills

--- src/components/Competences.jsx
+++ src/components/Competences.jsx
@@ -1,4 +1,4 @@
-import { skills } from "../data.js"
+import { skills } from "../data.js";
 import React, { useEffect, useRef, useState } from "react";
 
 export default function Competences() {
@@ -6,46 +6,54 @@ export default function Competences() {
   const sectionRef = useRef(null);
 
   useEffect(() => {
-      const observer = new IntersectionObserver(
+    const observer = new IntersectionObserver(
       (entries) => {
-          const [entry] = entries;
-          if (entry.isIntersecting) {
+        const [entry] = entries;
+        if (entry.isIntersecting) {
           setIsVisible(true);
           observer.unobserve(sectionRef.current); // Stop observing after it becomes visible
-          }
+        }
       },
       {
-          threshold: 0.5
-          , // Trigger when 10% of the section is visible
+        threshold: 0.5, // Trigger when 10% of the section is visible
       }
-      );
+    );
 
-      if (sectionRef.current) {
+    if (sectionRef.current) {
       observer.observe(sectionRef.current);
-      }
+    }
 
-      return () => {
+    return () => {
       if (sectionRef.current) {
-          observer.unobserve(sectionRef.current);
+        observer.unobserve(sectionRef.current);
       }
-      };
+    };
   }, []);
-  return (    
-    <section id="competences" 
-    ref={sectionRef} 
-    className={`py-8 flex flex-col items-center mt-24 mb-24 ${
-      isVisible ? "opacity-100 animate-fade-in-down" : "opacity-0"
-    }`} 
+  return (
+    <section
+      id="competences"
+      ref={sectionRef}
+      className={`py-8 flex flex-col items-center mt-24 mb-24 ${
+        isVisible ? "opacity-100 animate-fade-in-down" : "opacity-0"
+      }`}
     >
       <h2 className="text-primary text-4xl font-bold  mb-6">Competences</h2>
-      
+
       <div className="container mt-6 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {Object.entries(skills).map(([key, value]) => (
-          <div className="text-primary p-4 shadow-lg rounded-lg"key={key}>
+          <div
+            className="border-2 border-white text-primary p-4 shadow-lg rounded-lg"
+            key={key}
+          >
             <h3 className="text-2xl font-bold  mb-4">{key}</h3>
             <div className="flex flex-wrap gap-4">
-              {value.map(skill => (
-                <img className="w-12 h-12" key={skill} src={`./icons/${skill}.svg`} alt={skill} />
+              {value.map((skill) => (
+                <img
+                  className="w-20 h-20"
+                  key={skill}
+                  src={`./icons/${skill}.svg`}
+                  alt={skill}
+                />
               ))}
             </div>
           </div>
--- src/data.js
+++ src/data.js
@@ -1,61 +1,59 @@
 export const projects = [
-    {
-      title: "AlhaIZ Breizh",
-      technologie : ["JS", "HTML/CSS", "POSTGRESQL", "PHP","DOCKER","Agile"],
-      description:
-        "Conception et développement d'une plateforme de location saisonnier pour une association.",
-      image: "./images/project-1.png",
-      link: "https://github.com/mdekerga/KOH_Sport",
-    },
-    {
-      title: "Actu Finances",
-      technologie : ["PYTHON","TKINTER","YFINANCE"],
-      description:
-        "Développement d'un logiciel d'exploitation d'une base de données d'une API pour donner les actualités d'une entreprise.  ",
-      image: "./images/project-2.png",
-      link: "https://github.com/mdekerga/Actu-Finances",
-    },
-    {
-      title: "Puissance 4",
-      technologie : ["C"],
-      description:
-        "Conception d'un jeu de puissance 4 en langage C pour que 2 personnes puissent y jouer.",
-      image: "./images/project-3.png",
-      link: "https://github.com/mdekerga/Puissance4",
-    },
-    {
-      title: "Portfolio",
-      technologie : ["REACT","TAILWIND"],
-      description:
-        "Le site sur lequel vous êtes.",
-      image: "./images/project-4.png",
-      link: "https://github.com/mdekerga/Portfolio",
-    },
-    {
-      title: "Timber",
-      technologie : ["C++","SFML"],
-      description:
-        "Recréation d'un jeu dans lequel il faut couper un arbre et esquiver les branches en C++",
-      image: "./images/project-5.jpeg",
-      link: "https://github.com/mdekerga/Timber",
-    },
-  ];
+  {
+    title: "AlhaIZ Breizh",
+    technologie: ["JS", "HTML/CSS", "POSTGRESQL", "PHP", "DOCKER", "Agile"],
+    description:
+      "Conception et développement d'une plateforme de location saisonnier pour une association.",
+    image: "./images/project-1.png",
+    link: "https://github.com/mdekerga/KOH_Sport",
+  },
+  {
+    title: "Actu Finances",
+    technologie: ["PYTHON", "TKINTER", "YFINANCE"],
+    description:
+      "Développement d'un logiciel d'exploitation d'une base de données d'une API pour donner les actualités d'une entreprise.  ",
+    image: "./images/project-2.png",
+    link: "https://github.com/mdekerga/Actu-Finances",
+  },
+  {
+    title: "Puissance 4",
+    technologie: ["C"],
+    description:
+      "Conception d'un jeu de puissance 4 en langage C pour que 2 personnes puissent y jouer.",
+    image: "./images/project-3.png",
+    link: "https://github.com/mdekerga/Puissance4",
+  },
+  {
+    title: "Portfolio",
+    technologie: ["REACT", "TAILWIND"],
+    description: "Le site sur lequel vous êtes.",
+    image: "./images/project-4.png",
+    link: "https://github.com/mdekerga/Portfolio",
+  },
+  {
+    title: "Timber",
+    technologie: ["C++", "SFML"],
+    description:
+      "Recréation d'un jeu dans lequel il faut couper un arbre et esquiver les branches en C++",
+    image: "./images/project-5.jpeg",
+    link: "https://github.com/mdekerga/Timber",
+  },
+];
 
 export const diplomes = [
   {
     date: "2022-Présent",
     ecole: "Bachelor Universitaire Technologique Informatique",
     lieu: "IUT de Lannion",
-    logo: "./images/Logo_IUT.png"
+    logo: "./images/Logo_IUT.png",
   },
   {
     date: "2022",
     ecole: "Baccalauréat Général",
     lieu: "Lycée-Notre dame du Kreisker",
-    logo: "./images/lycee.png"
+    logo: "./images/lycee.png",
   },
-
-]
+];
 
 export const experiences = [
   {
@@ -63,17 +61,18 @@ export const experiences = [
     dateFin: "05/25",
     emploi: "Stage Développeur Front",
     entreprise: "Brittany Ferries",
-    description:"Conception et développement d’un outil d’analyse de l’accessibilité des applications internes de l’entreprise.",
+    description:
+      "Conception et développement d’un outil d’analyse de l’accessibilité des applications internes de l’entreprise.",
     lieu: "Roscoff",
-    technologie: ["node","typescript","playwright"],
-    logo: "./images/bf.png"
+    technologie: ["node", "typescript", "playwright"],
+    logo: "./images/bf.png",
   },
-]
+];
 
 export const skills = {
-  FrontEnd : ["html","css"],
-  BackEnd : ["php","postgresql"],
-  Programmation : ["cpp","java","python"],
-  Framework : ["django","react","laravel"],
-  Outils : ["docker","github"]
-}
+  FrontEnd: ["html", "css", "javascript"],
+  BackEnd: ["php", "postgresql", "mongodb"],
+  Programmation: ["cpp", "java", "python"],
+  Framework: ["django", "react", "laravel", "spring"],
+  Environnement: ["vsc", "docker", "github"],
+};
